2e84a9183cacc0de6a3b0a7b064ba165e5261cda
Micca <micksam7@users.noreply.github.com>
2024-05-21 20:50:44 -0400

    Add Twist Relaxer stub for FIK 1.9

---
+++ RootMotion/FinalIK/IK Solvers/TwistRelaxer.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace RootMotion.FinalIK
+{
+    public class TwistRelaxer : MonoBehaviour
+    {
+        public IK ik;
+        public Transform parent;
+        public Transform child;
+        public float weight = 1f;
+        public float parentChildCrossfade = 0.5f;
+        public float twistAngleOffset;
+    }
+}
